4c1496a1a20106bcf4e2d48df5ff267382017ff8
Kai Schuyler <kschuygon@zephyntosh-3.local>
2021-09-08 03:38:29 -0600

    added recording and colors

---
+++ lab1_MobileApp/README.md
@@ -0,0 +1,12 @@
+# Lab 1
+### ATLS 4120 Mobile App Development
+
+## Instructions
+Create an app in a new Xcode project that includes the following:
+- at least two buttons that change both an image and some text when tapped
+- adaptive for all size iPhones in portrait orientation
+- app icons
+- launch screen
+
+## Recording
+![recording](recording.gif)
--- lab1_MobileApp/lab1_MobileApp/ViewController.swift
+++ lab1_MobileApp/lab1_MobileApp/ViewController.swift
@@ -14,17 +14,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet var viewController: UIView!
     @IBOutlet var albumCover: UIImageView!
+    @IBOutlet var glassAnimalsLabel: UILabel!
     
     @IBAction func dreamlandButton(_ sender: Any) {
         albumCover.image = UIImage(named: "dreamland")
+        viewController.backgroundColor = UIColor(named: "dreamlandBg")
+        glassAnimalsLabel.textColor = UIColor(named: "dreamlandFg")
     }
     @IBAction func humanButon(_ sender: Any) {
         albumCover.image = UIImage(named: "howToBeAHumanBeing")
+        viewController.backgroundColor = UIColor(named: "humanBg")
+        glassAnimalsLabel.textColor = UIColor(named: "humanFg")
     }
     @IBAction func zabaButton(_ sender: Any) {
         albumCover.image = UIImage(named: "zaba")
+        viewController.backgroundColor = UIColor(named: "zabaBg")
+        glassAnimalsLabel.textColor = UIColor(named: "zabaFg")
     }
-    
 }
 
